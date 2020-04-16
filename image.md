9c0035e77b4e1e221e242a6a9249d982d31c6f7b
Lilian Ovrawah <lilianovrawah@gmail.com>
2020-04-16 19:06:19 +0100

    crud

---
+++ image.md
@@ -0,0 +1,8 @@
+# movies inserted
+![movie collection] (https://res.cloudinary.com/dzfz6iwon/image/upload/v1587060400/movie_collrction_fy4eji.png)
+
+# movie titles
+![movie titles] (https://res.cloudinary.com/dzfz6iwon/image/upload/v1587060255/collection_of_movie_uaniwp.png)
+
+# update interns
+![update interns] (https://res.cloudinary.com/dzfz6iwon/image/upload/v1587061028/updateinterns_y2wqnq.png)
--- interns.js
+++ interns.js
@@ -1,13 +1,28 @@
 var MongoClient = require('mongodb').MongoClient;
+const findDocuments = require('./findInterns');
+const updateDocuments = require('./updateInterns')
 var url = "mongodb://localhost:27017/";
 
 MongoClient.connect(url, function(err, db) {
   if (err) throw err;
-  var dbo = db.db("mydb");
-  dbo.createCollection("interns", function(err, res) {
+  var dbo = db.db("leeCodes");
+  const movies = [
+              { movie: "The Banker", year: "2020", rating: "8" },
+              { movie: "Bad Boys", year: "2020", rating: "7" },
+              { movie: "The Hunt", year: "2020", rating: "7" },
+              { movie: "Bloodshot", year: "2020", rating: "7.5" },
+              { movie: "First Cow", year: "2020", rating: "6.5" }
+              ]
+  dbo.createCollection("myMovies", function(err, res) {
     if (err) throw err;
     console.log("Interns collection created!");
-    db.close();
+    dbo.collection("myMovies").insertMany(movies, function (err, res) {
+      if (err) throw err;
+      console.log(res)
+      console.log("Number of movies inserted are: " + res.insertedCount);
+      db.close();
+      
+    });
   });
 });
 
